834c98b048a4102e1c169555c198f106ff9e512e
truongductri <clevolove.unik37@gmail.com>
2018-09-21 16:03:34 +0700

    touachable to ripple within border(false), without border(true)

--- Navigation/App.js
+++ Navigation/App.js
@@ -1,85 +1,97 @@
 import React, { Component } from 'react';
-import { Text, Button, StatusBar, TextInput, KeyboardAvoidingView, View, StyleSheet } from 'react-native';
+import {
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  Platform,
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
 
+
+const SUPPORTS_NATIVE_FEEDBACK =
+  Platform.OS === 'android' && Platform.Version >= 21;
+
+const noop = () => {};
+const defaultHitSlop = { top: 15, bottom: 15, right: 15, left: 15 };
 
 export default class App extends Component {
-  state = {
-    email: '',
-  };
-  
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar barStyle="light-content" />
-        <View style={styles.header}>
-          <Text style={styles.description}>
-            This demo shows how to avoid covering important UI elements with the software keyboard.
-            Focus the email input below and notice that the Sign Up button and the text adjusted
-            positions to make sure they are not hidden under the keyboard.
-          </Text>
-        </View>
-        <KeyboardAvoidingView behavior="padding" style={styles.form}>
-          <TextInput
-            style={styles.input}
-            value={this.state.email}
-            onChangeText={email => this.setState({email})}
-            ref={ref => {this._emailInput = ref}}
-            placeholder="email@example.com"
-            autoCapitalize="none"
-            autoCorrect={false}
-            keyboardType="email-address"
-            returnKeyType="send"
-            onSubmitEditing={this._submit}
-            blurOnSubmit={true}
-          />
-          <View>
-            <Button title="Sign Up" onPress={this._submit} />
-            <Text style={styles.legal}>
-              Some important legal fine print here
-            </Text>
-          </View>
-        </KeyboardAvoidingView>
+        {!SUPPORTS_NATIVE_FEEDBACK && this._renderButtons()}
+        {SUPPORTS_NATIVE_FEEDBACK && this._renderButtonsWithNativeFeedback()}
       </View>
     );
   }
-  
-  _submit = () => {
-    alert(`Confirmation email has been sent to ${this.state.email}`);
-  };
+
+  _renderButtons() {
+    return (
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={noop}
+          hitSlop={defaultHitSlop}>
+          <Text style={styles.text}>This is opacity</Text>
+        </TouchableOpacity>
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={noop}
+          hitSlop={defaultHitSlop}
+          underlayColor="red">
+          <Text style={styles.text}>This is highlight</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+
+  _renderButtonsWithNativeFeedback() {
+    return (
+      <View style={styles.buttonContainer}>
+        <TouchableNativeFeedback
+          onPress={noop}
+          background={TouchableNativeFeedback.Ripple('blue', false)}
+          hitSlop={defaultHitSlop}>
+          <View style={styles.button}>
+            <Text style={styles.text}>This is a ripple respecting borders</Text>
+          </View>
+        </TouchableNativeFeedback>
+
+        <TouchableNativeFeedback
+          onPress={noop}
+          background={TouchableNativeFeedback.Ripple('red', true)}
+          hitSlop={defaultHitSlop}>
+          <View style={styles.button}>
+            <Text style={styles.text}>This is ripple without borders, this is more useful for icons, eg: in tab bar</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ecf0f1',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 20,
+    backgroundColor: '#fff',
   },
-  header: {
-    paddingTop: 20 ,
-    padding: 20,
-    backgroundColor: '#336699',
+  buttonContainer: {
+    margin: 24,
   },
-  description: {
-    fontSize: 14,
-    color: 'white',
+  text: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
   },
-  input: {
-    margin: 20,
-    marginBottom: 0,
-    height: 34,
-    paddingHorizontal: 10,
-    borderRadius: 4,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    fontSize: 12,
-  },
-  legal: {
-    margin: 10,
-    color: '#333',
-    fontSize: 12,
-    textAlign: 'center',
-  },
-  form: {
-    flex: 1,
-    justifyContent: 'space-between',
+  button: {
+    padding: 25,
+    borderRadius: 5,
+    backgroundColor: '#000',
+    marginBottom: 30,
   },
 });
